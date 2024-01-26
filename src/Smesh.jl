f0a5a35e097d47e06e38157fa7ac29868aef7a2a
Benjamin Bolm <benjamin.bolm@gmx.de>
2024-01-26 13:24:49 +0100

    Add `delaunay_compute_neighbors` routine

--- src/Smesh.jl
+++ src/Smesh.jl
@@ -1,8 +1,8 @@
 module Smesh
 
-using Preferences: @load_preference, @has_preference 
+using Preferences: @load_preference, @has_preference
 
-export build_delaunay_triangulation
+export build_delaunay_triangulation, delaunay_compute_neighbors
 
 if !@has_preference("libsmesh")
     error("""
@@ -23,27 +23,39 @@ if !@has_preference("libsmesh")
 end
 const libsmesh = @load_preference("libsmesh")
 
-
 """
 """
 function build_delaunay_triangulation(data_points; shuffle = false, verbose = false)
     # Pre-allocate output array
     npoints = size(data_points, 2)
     ve_max = @ccall libsmesh.delaunay_triangulation_temparray_size_c(npoints::Cint)::Cint
-    ve_internal = Matrix{Cint}(undef, 3, ve_max)
+    ve_out = Matrix{Cint}(undef, 3, ve_max)
 
     # Perform triangulation
-    ntriangles = @ccall libsmesh.build_delaunay_triangulation_c(ve_internal::Ref{Cint},
+    ntriangles = @ccall libsmesh.build_delaunay_triangulation_c(ve_out::Ref{Cint},
                                                                 data_points::Ref{Float64},
                                                                 npoints::Cint,
                                                                 ve_max::Cint,
                                                                 shuffle::Cint,
                                                                 verbose::Cint)::Cint
 
-    # Copy to array of appropriate size and convert to Julia `Int`s for convenience
-    ve_out = convert(Matrix{Int}, ve_internal[:, 1:ntriangles])
+    # Resize array to appropriate size
+    # TODO: Converting to Julia `Int`s for convenience.
+    ve_out = ve_out[:, 1:ntriangles]
 
     return ve_out
 end
 
+"""
+"""
+function delaunay_compute_neighbors(data_points, vertices)
+    n_nodes = size(data_points, 2)
+    n_elements = size(vertices, 2)
+    neighbors = Matrix{Cint}(undef, 3, n_elements)
+
+    @ccall libsmesh.delaunay_compute_neighbors_c(neighbors::Ref{Cint}, vertices::Ref{Cint},
+                                                 n_elements::Cint, n_nodes::Cint)::Cvoid
+
+    return neighbors
+end
 end # module Smesh
